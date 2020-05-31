ada4a9397b43c2c11d5c01e304463484c13d6c55
Elariondakta <prevottheodore@gmail.com>
2020-05-31 02:30:46 +0200

    disp library

--- lib/views/library_view/library_body_view.dart
+++ lib/views/library_view/library_body_view.dart
@@ -1,11 +1,17 @@
+import 'dart:ui';
+
 import 'package:Videotheque/components/divider_component.dart';
 import 'package:Videotheque/components/skeleton_carrousel_component.dart';
 import 'package:Videotheque/controllers/library_controller/library_body_controller.dart';
 import 'package:Videotheque/globals.dart';
+import 'package:Videotheque/utils.dart';
 import 'package:Videotheque/views/library_view/card_view.dart';
 import 'package:Videotheque/views/library_view/carrousel_view.dart';
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:progressive_image/progressive_image.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 
 class LibraryBodyView extends StatelessWidget {
   QueryTypes type;
@@ -13,7 +19,7 @@ class LibraryBodyView extends StatelessWidget {
   Color splashColor;
 
   LibraryBodyView(this.type) {
-    mainColor = GlobalsMessage.chipData[QueryTypes.values.indexOf(type)]["color"];
+    mainColor = Colors.grey[800];
     splashColor = GlobalsMessage.chipData[QueryTypes.values.indexOf(type)]["splash_color"];
   }
 
@@ -26,9 +32,11 @@ class LibraryBodyView extends StatelessWidget {
         physics: BouncingScrollPhysics(),
 
         slivers: [
-          SliverPadding(padding: EdgeInsets.only(top: 20)),
           controller.dispElement(ElementsTypes.ToSeeCarrousel) ? 
-            SliverToBoxAdapter(child: DividerComponent(mainColor, type == QueryTypes.all ? "À voir" : "${GlobalsMessage.chipData[QueryTypes.values.indexOf(type)]["keyword"]} à voir")) 
+            SliverPadding(
+              padding: const EdgeInsets.only(top: 20),
+              sliver: SliverToBoxAdapter(child: DividerComponent(mainColor, type == QueryTypes.all ? "À voir" : "${GlobalsMessage.chipData[QueryTypes.values.indexOf(type)]["keyword"]} à voir")),
+            ) 
             : SliverPadding(padding: EdgeInsets.zero),
           SliverToBoxAdapter(
             child: Container(
@@ -111,6 +119,67 @@ class LibraryBodyView extends StatelessWidget {
               ),
             ),
           ),
+          controller.dispElement(ElementsTypes.MainData) ? SliverStickyHeaderBuilder(
+            controller: controller.libraryHeaderController..addListener(() {print(controller.libraryHeaderController.stickyHeaderScrollOffset);}),
+            builder: (BuildContext context, SliverStickyHeaderState state) => AppBar(
+              backgroundColor: state.isPinned ? Colors.white.withOpacity(0.4) : Colors.transparent,
+              elevation: state.isPinned ? 4 : 0,
+              titleSpacing: 0,
+              title: ClipRect(
+                child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8), child: AppBar(
+                  backgroundColor: Colors.white.withOpacity(0.4),
+                  elevation: 0,
+                  title: Text(GlobalsMessage.chipData[QueryTypes.values.indexOf(type)]["library_appBar"], style: TextStyle(color: Colors.grey[800])),
+                  actions: <Widget>[
+                    IconButton(icon: Icon(Icons.search, color: Colors.grey[800], size: 30)),
+                    Padding(padding: EdgeInsets.only(right: 5)),
+                    IconButton(icon: Icon(CommunityMaterialIcons.view_list, color: Colors.grey[800], size: 30)),
+                    Padding(padding: EdgeInsets.only(right: 5)),
+                    IconButton(icon: Icon(CommunityMaterialIcons.view_grid, color: Colors.grey[800], size: 30)),
+                    Padding(padding: EdgeInsets.only(right: 16))
+                  ],
+                )),
+              ),
+            ),
+            sliver: SliverPadding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
+              sliver: SliverGrid.count(
+                crossAxisCount: 2,
+                // maxCrossAxisExtent: MediaQuery.of(context).size.width/2,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                childAspectRatio: 0.67,
+                children: List.generate(controller.libraryLength, (index) {
+                  String heroTag = controller.heroTag;
+                  return RaisedButton(
+                    elevation: 2,
+                    clipBehavior: Clip.hardEdge,
+                    onPressed: () => controller.onElementTapped(index, heroTag),
+                    highlightElevation: 4,
+                    padding: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                    child: Stack(
+                      children: <Widget>[
+                        Hero(
+                          tag: heroTag,
+                          child: ProgressiveImage(
+                            placeholder: AssetImage("assets/loading.png"),
+                            thumbnail: Utils.fetchImage(controller.getImageElement(index), controller.getImageType(index), true),
+                            image: Utils.fetchImage(controller.getImageElement(index), controller.getImageType(index)),
+                            width: 480,
+                            height: 600,
+                            fit: BoxFit.fitHeight,
+                            fadeDuration: Duration(milliseconds: 150),
+                            blur: 2,                      
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                })
+              ),
+            )
+          ) : SliverToBoxAdapter(child: Container())
         ]
       )),
     );
