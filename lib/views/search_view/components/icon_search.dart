0d06a4d99ccf90486ce2ac93c73e9c06537c53e5
Elariondakta <prevottheodore@gmail.com>
2020-06-02 11:11:42 +0200

    staggered animation replacement

--- lib/views/search_view/components/icon_search.dart
+++ lib/views/search_view/components/icon_search.dart
@@ -1,8 +1,6 @@
-import 'package:Videotheque/controllers/search_controller/icon_search.dart';
 import 'package:Videotheque/globals.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 
 class IconSearchView extends StatelessWidget {
@@ -10,39 +8,40 @@ class IconSearchView extends StatelessWidget {
   IconSearchView(this.type);
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<IconSearchController>(
-      create: (context) => IconSearchController(),
-      child: Consumer<IconSearchController>(builder: (context, controller, child) {
-        return AnimatedOpacity(
-          opacity: controller.dispAnimation ? 1 : 0,
-          duration: controller.animationDelay, 
-          curve: Curves.ease,
-          child: AnimatedContainer(
-            duration: controller.animationDelay,
-            curve: Curves.ease,
-            transform: controller.dispAnimation ? Matrix4.translationValues(0, 0, 0) : Matrix4.translationValues(0, 50, 0),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 65, bottom: 25), 
-                  child: Center(
-                    child: Icon(GlobalsMessage.chipData[QueryTypes.values.indexOf(type)]["icon"],
-                      size: 100
-                    )
+    return Column(
+      children: <Widget>[
+        AnimationConfiguration.staggeredList(
+          position: 0,
+          duration: const Duration(milliseconds: 350),
+          child: SlideAnimation(
+            verticalOffset: 100,
+            child: FadeInAnimation(
+              child: Padding(
+                padding: EdgeInsets.only(top: 65, bottom: 25), 
+                child: Center(
+                  child: Icon(GlobalsMessage.chipData[QueryTypes.values.indexOf(type)]["icon"],
+                    size: 100
                   )
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Text(GlobalsMessage.defaultSearchMessage,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
-                  ),
                 )
-              ],
+              ),
             ),
           ),
-        );
-      })
+        ),
+        AnimationConfiguration.staggeredList(
+          position: 1,
+          duration: const Duration(milliseconds: 350),
+          child: SlideAnimation(
+            verticalOffset: 100,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(GlobalsMessage.defaultSearchMessage,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- lib/views/search_view/components/loading.dart
+++ lib/views/search_view/components/loading.dart
@@ -1,6 +1,5 @@
-import 'package:Videotheque/controllers/search_controller/loading_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:skeleton_text/skeleton_text.dart';
 
 class LoadingView extends StatelessWidget {
@@ -9,105 +8,91 @@ class LoadingView extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<LoadingController>(
-      create: (context) => LoadingController(),
-      child: SafeArea(
-          top: false,
-          bottom: false,
-          child: Builder(builder: (BuildContext context) {
-            return CustomScrollView(
-              physics: BouncingScrollPhysics(),
-              slivers: <Widget>[
-                SliverOverlapInjector(
-                  handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-                ),
-                SliverList(
-                  delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
-                    return Consumer<LoadingController>(
-                      builder: (context, controller, child) { 
-                        return AnimatedOpacity(
-                          opacity: controller.dispAnimation ? 1 : 0,
-                          duration: controller.animationDelay, 
-                          curve: Curves.ease,
-                          child: AnimatedContainer(
-                            duration: controller.animationDelay,
-                            curve: Curves.ease,
-                            transform: controller.dispAnimation ? Matrix4.translationValues(0, 0, 0) : Matrix4.translationValues(0, 50, 0),
-                            child: Card(
-                              elevation: 2,
-                              borderOnForeground: true,
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
-                              margin: EdgeInsets.all(6),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  ListTile( //Skeleton for title
-                                    title: SkeletonAnimation(child: Container(
-                                      width: MediaQuery.of(context).size.width*0.7, 
-                                      height: 25, 
-                                      decoration: BoxDecoration(
-                                        color: Colors.grey[300],
-                                        borderRadius: BorderRadius.all(Radius.circular(6)),
-                                      ),
-                                    )),
-                                  ),
-                                  Container(
-                                    padding: EdgeInsets.only(right: 7, left: 7, bottom: 7, top:0),
-                                    child: Row(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      mainAxisSize: MainAxisSize.max,
-                                      children: <Widget>[ //Skeleton for image
-                                        SkeletonAnimation(
-                                          child: Container(
-                                            width: 100,
-                                            height: 150,
-                                            color: Colors.grey[300],
-                                          ),
-                                        ),
-                                        Padding(  //Skeleton for text
-                                          padding: EdgeInsets.only(left: 20, top: 8),
-                                          child: Column(children: List.generate(7, (int index) {
-                                            return Padding(padding: EdgeInsets.only(bottom: 7), child: Theme(
-                                              data: ThemeData(backgroundColor: Colors.grey[300]),
-                                              child: SkeletonAnimation(
-                                                child: Container(
-                                                  width: MediaQuery.of(context).size.width*0.55, 
-                                                  height: 13,
-                                                  decoration: BoxDecoration(
-                                                    color: Colors.grey[300],
-                                                    borderRadius: BorderRadius.all(Radius.circular(4)),
-                                                  ),
-                                                )
-                                              )
-                                            ));
-                                          }
-                                        )
-                                      ),            
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Divider(
-                                    color: baseColor,
-                                    height: 2,
-                                    thickness: 2,
-                                  ),
-                                ],
+      return CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: <Widget>[
+          SliverOverlapInjector(
+            handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+              return AnimationConfiguration.staggeredList(
+                duration: const Duration(milliseconds: 350),
+                position: index,
+                child: SlideAnimation(
+                  verticalOffset: 25,
+                  child: FadeInAnimation(
+                    child: Card(
+                      elevation: 2,
+                      borderOnForeground: true,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                      margin: EdgeInsets.all(6),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          ListTile( //Skeleton for title
+                            title: SkeletonAnimation(child: Container(
+                              width: MediaQuery.of(context).size.width*0.7, 
+                              height: 25, 
+                              decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.all(Radius.circular(6)),
                               ),
+                            )),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(right: 7, left: 7, bottom: 7, top:0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[ //Skeleton for image
+                                SkeletonAnimation(
+                                  child: Container(
+                                    width: 100,
+                                    height: 150,
+                                    color: Colors.grey[300],
+                                  ),
+                                ),
+                                Padding(  //Skeleton for text
+                                  padding: EdgeInsets.only(left: 20, top: 8),
+                                  child: Column(children: List.generate(7, (int index) {
+                                    return Padding(padding: EdgeInsets.only(bottom: 7), child: Theme(
+                                      data: ThemeData(backgroundColor: Colors.grey[300]),
+                                      child: SkeletonAnimation(
+                                        child: Container(
+                                          width: MediaQuery.of(context).size.width*0.55, 
+                                          height: 13,
+                                          decoration: BoxDecoration(
+                                            color: Colors.grey[300],
+                                            borderRadius: BorderRadius.all(Radius.circular(4)),
+                                          ),
+                                        )
+                                      )
+                                    ));
+                                  }
+                                )
+                              ),            
+                                ),
+                              ],
                             ),
-                          )
-                        );
-                      }
-                    );
-                  },
-                  childCount: 10
+                          ),
+                          Divider(
+                            color: baseColor,
+                            height: 2,
+                            thickness: 2,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              )
-            ]
-          );
-        },
-      ),
-    ));
+              );
+            },
+            childCount: 10
+          ),
+        )
+      ]
+    );
   }
 }
