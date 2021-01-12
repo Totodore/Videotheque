e5448304a184e8c13d1c1f90fae3ba030091427c
Théodore Prévot <prevottheodore@gmail.com>
2021-01-12 03:12:45 +0100

    barcode update (more precise)

--- lib/services/BarcodeLookup.dart
+++ lib/services/BarcodeLookup.dart
@@ -1,15 +1,17 @@
+import 'dart:collection';
+import 'dart:convert';
 import 'dart:io';
-
+import 'package:path/path.dart' as p;
 import 'package:Videotheque/Env.dart';
 import 'package:http/http.dart';
-import 'package:html/parser.dart' show parse;
 
 class BarcodeLookup {
 
-  final regex = new RegExp(r"\[(.*?)\]|\((.*?)\)", multiLine: false, caseSensitive: false);
+  final regex = new RegExp(r"[\+\[\]=-]", multiLine: false, caseSensitive: false);
   
   Future<String> getTitle(String barcode) async {
-    Uri url = new Uri.https(environment.BARCODE_ORIGIN, barcode);
+    Uri url = new Uri.https(environment.BARCODE_ORIGIN, p.join("api/v1/search"), { "query": barcode });
+    print(url);
     Request req = new Request("GET", url);
     req.headers.addAll({
       "authority": environment.BARCODE_ORIGIN,
@@ -17,8 +19,15 @@ class BarcodeLookup {
     });
     final res = await req.send();
     if (res.statusCode == 200) {
-      final html = parse(await res.stream.bytesToString());
-      return html.querySelector(".detailtitle h2").innerHtml.replaceAll(regex, "");
+      HashMap<String, dynamic> data = HashMap.from(jsonDecode(await res.stream.bytesToString())[0]);
+      String title = "";
+      for(final w in data["title"].split(" ")) {
+        if (w.contains(regex))
+          break;
+        else
+          title += " " + w;
+      }
+      return title;
     }
     else throw HttpException;
   }
