8dcec34a2cfaa20d15666b24faa19fde1f0ec653
Théodore Prévot <prevottheodore@gmail.com>
2021-01-15 14:58:09 +0100

    search to CamelCase

---
+++ lib/controllers/search_controller/SearchController.dart
@@ -0,0 +1,167 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:Videotheque/services/BarcodeLookup.dart';
+import 'package:Videotheque/services/FireauthQueries.dart';
+import 'package:Videotheque/services/FireconfigQueries.dart';
+import 'package:Videotheque/services/FirestoreQueries.dart';
+import 'package:Videotheque/Globals.dart';
+import 'package:Videotheque/services/TmdbQueries.dart';
+import 'package:Videotheque/utils/Singletons.dart';
+import 'package:barcode_scan_fix/barcode_scan.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class SearchController extends ChangeNotifier {
+  final focusNode = FocusNode();
+  final searchInputController = TextEditingController();
+  final tabLength = QueryTypes.values.length;
+  final BuildContext context;
+  String barcode;
+
+  Map<QueryTypes, States> loadedViews = Map.fromIterables(QueryTypes.values, List.generate(QueryTypes.values.length, (int index) => States.Nothing)); //List which represent each view if it has been loaded
+  //List which represent offset of result per view
+  Map<QueryTypes, List> dataLoadedView = Map.fromIterables(QueryTypes.values, List.generate(QueryTypes.values.length, (int index) => null)); 
+
+  QueryTypes selectedSort = QueryTypes.all;
+  int tabIndex = 0;
+  TabController resultsPageController;
+  Timer delayOkSearch;
+  bool innerBoxScrolled;
+  bool test = true;
+  String actualQuery = "";
+  bool triggerChangeListener = false;
+
+  FireauthQueries fireauth = Singletons.instance<FireauthQueries>();
+  FirestoreQueries firestore = Singletons.instance<FirestoreQueries>();
+  FireconfigQueries fireconfig = Singletons.instance<FireconfigQueries>();
+  TMDBQueries tmdbQueries = Singletons.instance<TMDBQueries>();
+
+  SearchController(this.context) {
+    focusNode.requestFocus();
+  }
+  
+  void searchQuery(String query) async {
+    QueryTypes searchingSelectedSort = selectedSort; //On sauvegarde le type actuel au cas ou l'utilisateur change d'index pdt le chargement
+    if (query == null || query.isEmpty) {
+      clearView();
+      notifyListeners();
+      return;
+    }
+    actualQuery = query;
+    loadedViews[searchingSelectedSort] = States.Loading;
+    notifyListeners();
+
+    Map<String, dynamic> result;
+    switch (searchingSelectedSort) {
+      case QueryTypes.all:
+        result = await tmdbQueries.onlineSearchMulti(query);
+        break;
+      case QueryTypes.movie:
+        result = await tmdbQueries.onlineSearchMovie(query);
+        break;
+      case QueryTypes.person:
+        result = await tmdbQueries.onlineSearchPerson(query);
+        break;
+      case QueryTypes.tv:
+        result = await tmdbQueries.onlineSearchTV(query);
+        break;
+      case QueryTypes.collection:
+        result = await tmdbQueries.onlineSearchCollection(query);
+        break;
+      // case QueryTypes.companies:
+        // result = await tmdbQueries.onlineSearchCompanies(query);
+        // break;
+    }
+
+    if (result["error"] != null) {
+      loadedViews[searchingSelectedSort] = States.Error;
+      notifyListeners();
+      return;
+    }
+    else if (result["results"] == null || List.from(result["results"]).length == 0) {
+      loadedViews[searchingSelectedSort] = States.Empty;
+      notifyListeners();
+      return;
+    }   
+    dataLoadedView[searchingSelectedSort] = result["results"];
+    loadedViews[searchingSelectedSort] = States.Added;
+    notifyListeners();
+  }
+
+  void onchangeQuery(String query) {
+    if (query == null || query.length == 0)
+      clearView();
+    if (delayOkSearch != null) {
+      delayOkSearch.cancel();
+    }
+    delayOkSearch = Timer(Duration(milliseconds: 500), () {delayOkSearch = null; searchQuery(query);});
+  }
+
+  void clearView() {
+    searchInputController.clear();
+    actualQuery = "";
+    dataLoadedView = Map.fromIterables(QueryTypes.values, List.generate(QueryTypes.values.length, (int index) => null));
+    loadedViews = Map.fromIterables(QueryTypes.values, List.generate(QueryTypes.values.length, (int index) => States.Nothing));
+    notifyListeners();
+  }
+
+  void updateTabIndex(int index) {
+    selectedSort = QueryTypes.values[index];
+    tabIndex = index;
+    notifyListeners();
+    if (loadedViews[QueryTypes.values[index]] == States.Nothing && actualQuery.length > 0) {
+      searchQuery(actualQuery);
+    }
+  }
+
+  List getDataResults(QueryTypes type) {
+    return dataLoadedView[type];
+  }
+
+  bool isChipSelected(int index) => GlobalsMessage.chipData[index]["type"] == selectedSort; 
+
+  void updateIsInnerBoxScrolled(bool isScrolled) {
+    innerBoxScrolled = isScrolled;
+    if (innerBoxScrolled)
+      focusNode.unfocus();
+  }
+
+  Future<void> barcodeScan() async {
+    if (!await _requestPermissions()) {
+      GlobalsFunc.snackBar(context, "L'accès à la caméra est désactivé");
+    } else
+      _scan();
+  }
+
+  
+  Future<bool> _requestPermissions() async {
+    PermissionStatus status = await Permission.camera.request();
+    return status == PermissionStatus.granted || status == PermissionStatus.restricted;
+  }
+
+  Future<void> _scan() async {
+    try {
+      barcode = await BarcodeScanner.scan();
+      searchInputController.text = await Singletons.instance<BarcodeLookup>().getTitle(barcode);
+      searchInputController.selection = TextSelection.collapsed(offset: searchInputController.text.length-1);
+      searchQuery(searchInputController.text);
+      notifyListeners();
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        GlobalsFunc.snackBar(context, "L'accès à la caméra est désactivé");
+      } else {
+        GlobalsFunc.snackBar(context);
+        print(e);
+      }
+    } on HttpException {
+      GlobalsFunc.snackBar(context, "Erreur lors de la récupération du film, réessayez...");
+    } on FormatException {
+      // GlobalsFunc.snackBar(context, "Erreur lors du scan, réessayez...");
+    } catch (e) {
+      GlobalsFunc.snackBar(context, "Erreur lors du scan du code barre, réessayez...");
+      print(e);
+    }
+  }
+}
