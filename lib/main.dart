721547458bccbdc875bbf67e46427a7f7147a896
Totodore <prevottheodore@gmail.com>
2021-01-28 16:45:01 +0100

    refactor appView

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:Videotheque/views/SplashScreenView.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'package:Videotheque/views/app_view.dart';
+import 'package:Videotheque/views/AppView.dart';
 import 'package:Videotheque/views/movie_view/movie_view.dart';
 import 'package:Videotheque/views/person_view/person_view.dart';
 import 'package:Videotheque/views/SearchView/SearchView.dart';
